1411dfb9100dbbc85b66547157b9cad7f4a716ec
saunghaymanoo <saunghaymanoo322@gmail.com>
2022-09-26 18:17:51 +0900

    category crud

--- src/components/DashboardNav.vue
+++ src/components/DashboardNav.vue
@@ -1,6 +1,6 @@
 <template>
             <!-- Navbar -->
-    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-2 border-radius-xl shadow-none" id="navbarBlur" data-scroll="true">
+    <nav class="navbar navbar-main navbar-expand-lg px-0 mx-2 my-4 border-radius-xl shadow-none" id="navbarBlur" data-scroll="true">
         <div class="container-fluid py-1 px-3">
         <nav aria-label="breadcrumb">
         <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
